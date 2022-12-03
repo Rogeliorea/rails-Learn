55356de2c31694d370815db0401eb790d4fbcd46
rogereur <>
2022-12-03 22:16:00 +0000

    Fix new home and devise working

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   # get 'home/index'
   get 'home/about'
   root 'home#index'
+
+  # sign out route
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+
 
 
 
